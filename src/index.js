483cfd19bb244d8021c6ea101bd9a2dc6c1563b5
Luca Luccioni <lucaluccioni@MacBookAir.homenet.telecomitalia.it>
2025-03-17 15:48:03 +0100

    Configured Router basename for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <App routes={routes} />
     </Router>
     
